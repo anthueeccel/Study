10962f9c2d02f987259dc8d0c4f9a3df557bc85b
anthueeccel <anthue@gmail.com>
2019-08-07 19:03:35 -0300

    add frontend to NodeJs-React

--- NodeJS/App1/backend/src/routes.js
+++ NodeJS/App1/backend/src/routes.js
@@ -1,15 +1,20 @@
-const expressa = require('express');
+const express = require('express'); //importa a variável express
+const routes = express.Router(); 
 
-const routes = express.Router();
+const DevController = require('./controllers/DevController');
+const LikeController = require('./controllers/LikeController');
+const DislikeController = require('./controllers/DislikeController');
 
-routes.get('/', (req, res) => {    
-    return res.json({ message: `Olá ${req.query.name}`});
-});
+// GET - página inicial
+//routes.get('/', (req, res) => {    
+//    return res.json({ message: `Olá ${req.query.name}`});
+//});
 
-routes.post('/devs', (req, res) => {
-    return res.json({ ok: true });
+// POST - recebe dados de um formulário (create)
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
 
-});
+routes.post('/devs/:devId/likes', LikeController.store);
+routes.post('/devs/:devId/dislikes', DislikeController.store);
 
-
-module.exports = routes;
+module.exports = routes; //exporta variável routes 
--- NodeJS/App1/backend/src/server.js
+++ NodeJS/App1/backend/src/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose'); //importação 
+const cors = require('cors');
+
+const routes = require('./routes'); //importa o arquivo routes.js
 
 const server = express();
 
-server.get('/', (req, res) => {
-    
-    return res.json({ message: `Olá ${req.query.name}`});
+mongoose.connect('mongodb+srv://node-app1:xcIwtJsBdLOhenHh@cluster0-3sp4a.mongodb.net/develop1?retryWrites=true&w=majority', {
+    useNewUrlParser: true
 });
-server.listen(3333);
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333); //porta no app/navegador
